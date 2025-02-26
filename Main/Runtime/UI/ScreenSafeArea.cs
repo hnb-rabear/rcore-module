fb6f3482602d617279c9598fe2a1cfcb9f9ece40
RadBear <nbhung71711@gmail.com>
2025-02-26 09:58:17 +0700

    Fix SafeScreen detector

--- Main/Runtime/UI/ScreenSafeArea.cs
+++ Main/Runtime/UI/ScreenSafeArea.cs
@@ -7,29 +7,23 @@ namespace RCore.UI
 {
 	public class ScreenSafeArea : MonoBehaviour
 	{
-		public static float topOffset;
-		public static float bottomOffset;
+		public float topOffset;
+		public float bottomOffset;
 		public Canvas canvas;
 		public RectTransform[] safeRects;
 		public bool fixedTop;
 		public bool fixedBottom;
-		private ScreenOrientation m_CurrentOrientation;
-		private Rect m_CurrentSafeArea;
 
 		private void Start()
 		{
-			m_CurrentOrientation = Screen.orientation;
-			m_CurrentSafeArea = Screen.safeArea;
 			CheckSafeArea();
 		}
 
-		// private void OnValidate()
-		// {
-		// 	if (Application.isPlaying)
-		// 		CheckSafeArea();
-		// }
-
+#if ODIN_INSPECTOR
 		[Button]
+#else
+		[InspectorButton]
+#endif
 		public void Log()
 		{
 			var safeArea = Screen.safeArea;
@@ -39,44 +33,49 @@ namespace RCore.UI
 			var oHeightTop = (Screen.currentResolution.height - safeArea.height - safeArea.y) / 2f;
 			var oWidthBot = -safeArea.x / 2f;
 			var oHeightBot = -safeArea.y / 2f;
-			Debug.Log($"Screen size: (width:{sWidth}, height:{sHeight})"
+			UnityEngine.Debug.Log($"Screen size: (width:{sWidth}, height:{sHeight})"
 				+ $"\nSafe area: {safeArea}"
 				+ $"\nOffset Top: (width:{oWidthTop}, height:{oHeightTop})"
 				+ $"\nOffset Bottom: (width:{oWidthBot}, height:{oHeightBot})");
 		}
 
+#if ODIN_INSPECTOR
+		[Button]
+#else
+		[InspectorButton]
+#endif
+		private void Validate()
+		{
+			CheckSafeArea();
+		}
+
 		private void CheckSafeArea()
 		{
 			var safeArea = Screen.safeArea;
-			safeArea.height -= topOffset;
-			var anchorMin = safeArea.position;
-			var anchorMax = safeArea.position + safeArea.size;
-			var sizeDelta = ((RectTransform)canvas.transform).sizeDelta;
-			sizeDelta.y = -bottomOffset;
-			((RectTransform)canvas.transform).sizeDelta = sizeDelta;
-			var position = ((RectTransform)canvas.transform).anchoredPosition;
-			position.y = bottomOffset / 2;
-			((RectTransform)canvas.transform).anchoredPosition = position;
+			safeArea.height -= topOffset + bottomOffset;
+			if (fixedTop)
+			{
+				safeArea.height += safeArea.y;
+			}
+			if (fixedBottom)
+			{
+				safeArea.height += safeArea.y;
+				safeArea.y = 0;
+			}
+			var anchorMin = safeArea.position + new Vector2(0, bottomOffset);
+			var anchorMax = safeArea.position + safeArea.size + new Vector2(0, bottomOffset);
 
-			anchorMin.x /= canvas.pixelRect.width;
-			anchorMin.y /= canvas.pixelRect.height;
-			anchorMax.x /= canvas.pixelRect.width;
-			anchorMax.y /= canvas.pixelRect.height;
+			var pixelRect = canvas.pixelRect;
+			anchorMin.x /= pixelRect.width;
+			anchorMin.y /= pixelRect.height;
+			anchorMax.x /= pixelRect.width;
+			anchorMax.y /= pixelRect.height;
 
 			foreach (var rect in safeRects)
 			{
-				if (!fixedBottom)
-					rect.anchorMin = anchorMin;
-				else
-					rect.anchorMin = Vector2.zero;
-				if (!fixedTop)
-					rect.anchorMax = anchorMax;
-				else
-					rect.anchorMax = Vector2.one;
+				rect.anchorMin = anchorMin;
+				rect.anchorMax = anchorMax;
 			}
-
-			m_CurrentOrientation = Screen.orientation;
-			m_CurrentSafeArea = Screen.safeArea;
 		}
 
 		public static void SetTopOffsetForBannerAd(float pBannerHeight, bool pPlaceInSafeArea = true)
@@ -94,15 +93,13 @@ namespace RCore.UI
 			{
 				offset = pBannerHeight;
 			}
-
-			if (topOffset == offset)
-				return;
-
-			topOffset = offset;
-
+			
 			var ScreenSafeAreas = FindObjectsOfType<ScreenSafeArea>();
 			foreach (var component in ScreenSafeAreas)
-				component.StartCoroutine(component.IECheckSafeArea());
+			{
+				component.topOffset = offset;
+				component.StartCoroutine(component.IEValidate());
+			}
 		}
 
 		public static void SetBottomOffsetForBannerAd(float pBannerHeight, bool pPlaceInSafeArea = true)
@@ -120,32 +117,34 @@ namespace RCore.UI
 			{
 				offset = pBannerHeight;
 			}
-
-			if (bottomOffset == offset)
-				return;
-
-			bottomOffset = offset;
-
+			
 			var screenSafeAreas = FindObjectsOfType<ScreenSafeArea>();
 			foreach (var component in screenSafeAreas)
-				component.StartCoroutine(component.IECheckSafeArea());
-		}
-
-		private IEnumerator IECheckSafeArea()
-		{
-			float time = 0.5f;
-			while (time > 0)
 			{
-				CheckSafeArea();
-				time -= Time.deltaTime;
-				yield return null;
+				component.bottomOffset = offset;
+				component.StartCoroutine(component.IEValidate());
 			}
 		}
 
+		private IEnumerator IEValidate()
+		{
+			Validate();
+			yield return null;
+			Validate();
+		}
+
+#if ODIN_INSPECTOR
 		[Button]
+#else
+		[InspectorButton]
+#endif
 		private void TestTopOffsetForBannerAd(int height) => SetTopOffsetForBannerAd(height);
 
+#if ODIN_INSPECTOR
 		[Button]
+#else
+		[InspectorButton]
+#endif
 		private void TestBottomOffsetForBannerAd(int height) => SetBottomOffsetForBannerAd(height);
 	}
 }
